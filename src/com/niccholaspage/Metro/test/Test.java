595cdd83ca4050138c5c22ae9ca7ac7657ba28b4
Nicholas Nassar <niccholaspage@gmail.com>
2013-02-07 20:32:48 -0500

    Add in logging for all online players

--- src/com/niccholaspage/Metro/test/Test.java
+++ src/com/niccholaspage/Metro/test/Test.java
@@ -1,11 +1,16 @@
 package com.niccholaspage.Metro.test;
 
 import com.niccholaspage.Metro.base.MetroPlugin;
+import com.niccholaspage.Metro.base.player.MetroPlayer;
 
 public class Test extends MetroPlugin {
 	public void onEnable(){
 		getLogger().info("Gaben");
 		
 		getServer().broadcastMessage("lol Gabe");
+		
+		for (MetroPlayer player : getServer().getOnlinePlayers()){
+			getLogger().info(player.getName());
+		}
 	}
 }
